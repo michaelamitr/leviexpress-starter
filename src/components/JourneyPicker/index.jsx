8fe43482bf9c55ab472b0bc600011473a98f2571
Michaela Trčková <misa.trckova@volny.cz>
2023-05-27 12:09:56 +0200

    3 Výběr data cesty

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -13,10 +13,18 @@ export const JourneyPicker = ({ onJourneyChange }) => {
 
   const [cities, setCities] = useState([]);
 
+  const [dates, setDates] = useState([]);
+
   useEffect(() => {
     fetch('https://apps.kodim.cz/daweb/leviexpress/api/cities')
       .then((response) => response.json())
       .then((data) => setCities(data.results));
+  }, []);
+
+  useEffect(() => {
+    fetch('https://apps.kodim.cz/daweb/leviexpress/api/dates')
+      .then((response) => response.json())
+      .then((data) => setDates(data.results));
   }, []);
 
   return (
@@ -39,12 +47,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
           <label>
             <div className="journey-picker__label">Datum:</div>
             <select onChange={(e) => setDate(e.target.value)}>
-              <option value="">Vyberte</option>
-              <option value="datum01">Datum 01</option>
-              <option value="datum02">Datum 02</option>
-              <option value="datum03">Datum 03</option>
-              <option value="datum04">Datum 04</option>
-              <option value="datum05">Datum 05</option>
+              <DatesOptions dates={dates} />
             </select>
           </label>
           <div className="journey-picker__controls">
@@ -64,8 +67,21 @@ const CityOptions = ({ cities }) => {
     <>
       <option value={''}>Vyberte</option>
       {cities.map((city) => (
-        <option key={city.code} value={city.code}>
+        <option key={city.code} value={city.name}>
           {city.name}
+        </option>
+      ))}
+    </>
+  );
+};
+
+const DatesOptions = ({ dates }) => {
+  return (
+    <>
+      <option value={''}>Vyberte</option>
+      {dates.map((date) => (
+        <option key={date.dateBasic} value={date.dateCs}>
+          {date.dateCs}
         </option>
       ))}
     </>
